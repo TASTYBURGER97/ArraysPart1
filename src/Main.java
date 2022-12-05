7a4970c7efeff8f819a0eeaabdec7a3aa09ed0be
TASTYBURGER97 <tihonov.artyom1997@yandex.ru>
2022-12-05 19:03:05 +0400

    проверьте пожалуйста задание 4. Консоль почему-то выдаёт ошибку

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
+        task3();
+        task4();
     }
 
 
@@ -49,5 +52,49 @@ public class Main {
 
 
     }
+
+    public static void task3() {
+        int[] arr1 = {1, 2, 3};
+        double[] arr2 = {1.57, 7.654, 9.986};
+        char[] arr3 = {'a', 'b', 'c', 'd', 'e'};
+
+        for (int i = arr1.length - 1; i >= 0; i--) {
+            System.out.println(arr1[i]);
+            if (i != 0) {
+                System.out.println(", ");
+            }
+        }
+        System.out.println();
+        for (int i = arr2.length - 1; i >= 0; i--) {
+            System.out.println(arr2[i]);
+            if (i != 0) {
+                System.out.println(", ");
+            }
+        }
+        System.out.println();
+        for (int i = arr3.length - 1; i >= 0; i--) {
+            System.out.println(arr3[i]);
+            if (i != 0) {
+                System.out.println(", ");
+            }
+        }
+        System.out.println();
+    }
+
+
+    public static void task4() {
+    int[] arr1 = {1, 2, 3};
+
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] % 2 != 0) {
+                arr1[i] += 1;
+        }
+    }
+        System.out.println(Arrays.toString(arr1);
+
+    }
 }
+
+
+
 
