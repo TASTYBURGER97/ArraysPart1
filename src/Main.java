31402af5946a38bfb83b2ab0225bbd8a5cff0a91
TASTYBURGER97 <tihonov.artyom1997@yandex.ru>
2022-12-05 17:28:56 +0400

    задания 1 и 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,53 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+
+
+    public static void task1() {
+        int[] arr1 = new int[3];
+        arr1[0] = 1;
+        arr1[1] = 2;
+        arr1[2] = 3;
+
+        double[] arr2 = {1.57, 7.654, 9.986};
+
+        char[] arr3 = {'a', 'b', 'c', 'd', 'e'};
+
+    }
+
+    public static void task2() {
+        int[] arr1 = {1, 2, 3};
+        double[] arr2 = {1.57, 7.654, 9.986};
+        char[] arr3 = {'a', 'b', 'c', 'd', 'e'};
+
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.println(arr1[i]);
+            if (i != arr1.length - 1) {
+                System.out.println(", ");
+            }
+        }
+        System.out.println();
+
+
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.println(arr2[i]);
+            if (i != arr2.length - 1) {
+                System.out.println(", ");
+            }
+        }
+        System.out.println();
+
+
+        for (int i = 0; i < arr3.length; i++) {
+            System.out.println(arr3[i]);
+            if (i != arr3.length - 1) {
+                System.out.println(", ");
+            }
+        }
+        System.out.println();
+
+
     }
 }
+
